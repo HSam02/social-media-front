38d078bfe3f87980934e358b3d296e0bad8d7231
Sam <samvel_2002@list.ru>
2023-01-18 13:25:19 +0400

    trying to fix cors from front 1

--- src/appAxios.js
+++ src/appAxios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const appAxios = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: { "Access-Control-Allow-Origin": "*" },
 });
 
 appAxios.interceptors.request.use((config) => {
